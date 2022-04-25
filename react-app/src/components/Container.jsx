f0321fbb5649d2d9e58095e40dd47641a8170160
dongntn <dongnguyen10102@gmail.com>
2022-04-25 16:22:24 +0700

    fhd

--- react-app/src/components/Container.jsx
+++ react-app/src/components/Container.jsx
@@ -7,6 +7,7 @@ import Slider from "react-slick"
 const Container = () => {
     const settings = {
         infinite: true,
+        dotsClass: "slick-dots slick-thumb",
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -15,15 +16,16 @@ const Container = () => {
     };
     return (
         <div class="grid gap-4 grid-cols-2 grid-rows-2 h-screen mt-6">
-            <div className='object-cover w-full h-full'>
+            <div className='object-cover w-full h-full overflow-hidden bg-cover '>
                 <Slider {...settings}>
-                    <div className='overflow-hidden bg-cover '   >
-                        <img  src="https://opacthuvien.ut.edu.vn/opac/Upload/ImageCover/116/Sach_moi_t6.JPG" alt="" />
+                    <div >
+                        <img className='object-cover w-full h-full overflow-hidden bg-cover' src="https://opacthuvien.ut.edu.vn/opac/Upload/ImageCover/116/Sach_moi_t6.JPG" alt="" />
                     </div>
                     <div>
-                        <h3>2</h3>
+                        <img src="https://opacthuvien.ut.edu.vn/opac/Upload/ImageCover/105/BST_sach_moi_thang_4.JPG" alt="" />
                     </div>
-                   
+                    <div>
+                        <img src="https://opacthuvien.ut.edu.vn/opac/Upload/ImageCover/117/sach_moi_t1.JPG" alt="" /></div>                   
                 </Slider>
             </div>
             <div className='grid grid-cols-3 grid-rows-2 gap-x-2 gap-y-2'>
@@ -97,8 +99,38 @@ const Container = () => {
 
 
             </div>
-            <div>guide</div>
-            <div>lastest</div>
+            <div>
+                    <p className='text-sky-900 ml-1 '>GUIDE</p>
+                <hr/>
+            </div>
+            <div>
+                    <p className='text-sky-900 ml-1 '>TAI LIEU MOI NHAT</p>
+                <hr/>
+                <div className='flex py-1  items-center m-1 '>
+                    <BookOpen  className='items-center justify-center p-1 m-1  bg-teal-400'/>
+                    <a href="/">The Elements of shipping / Alan E. Branch</a>
+                </div>
+                <div className='flex py-1  items-center m-1 '>
+                    <BookOpen  className='items-center justify-center p-1 m-1  bg-teal-400'/>
+                    <a href="/">The Elements of shipping / Alan E. Branch</a>
+                </div>
+                <div className='flex py-1  items-center m-1 '>
+                    <BookOpen  className='items-center justify-center p-1 m-1  bg-teal-400'/>
+                    <a href="/">The Elements of shipping / Alan E. Branch</a>
+                </div>
+                <div className='flex py-1  items-center m-1 '>
+                    <BookOpen  className='items-center justify-center p-1 m-1  bg-teal-400'/>
+                    <a href="/">The Elements of shipping / Alan E. Branch</a>
+                </div>
+                <div className='flex py-1  items-center m-1 '>
+                    <BookOpen  className='items-center justify-center p-1 m-1  bg-teal-400'/>
+                    <a href="/">The Elements of shipping / Alan E. Branch</a>
+                </div>
+                <div className='flex py-1  items-center m-1 '>
+                    <BookOpen  className='items-center justify-center p-1 m-1  bg-teal-400'/>
+                    <a href="/">The Elements of shipping / Alan E. Branch</a>
+                </div>
+            </div>
         </div>
     )
 }
